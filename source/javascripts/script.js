b106eddb22332bc5518f64effd376dd93b2dbdc2
Curtis Lu <karasugummi@gmail.com>
2023-09-22 16:02:21 +0900

    changed nav circles to left

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -4,6 +4,7 @@ new fullpage('#fullpage', {
   scrollHorizontally: true,
   sectionsColor : ['#fe5f46', '#0998ec', '#fc6c7c', '#fec304'],
   navigation: true,
+  navigationPosition: 'left',
   slidesNavigation: true,
   anchors:['firstPage', 'secondPage', 'thirdPage', 'finalPage'],
   navigationTooltips: ['Home', 'About Me', 'Projects', 'Contact Me']
